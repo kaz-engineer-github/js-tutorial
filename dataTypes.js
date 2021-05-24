46f119979d6d5b2261454b19d1f5af904043c941
kaz <kazufumi2468australia@gmail.com>
2021-05-24 09:55:38 +0900

    learn data types

---
+++ dataTypes.js
@@ -0,0 +1,7 @@
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ? = I think this result is just 1
+
+alert( `hello ${"name"}` ); // ? = I think this result is just name
+
+alert( `hello ${name}` ); // ? = I think this result is Ilya
