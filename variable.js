cd76e7b14816d224e1007ae6908f2a12b2af98b6
kaz <kazufumi2468australia@gmail.com>
2021-05-24 09:52:19 +0900

    learn variable name and identifier

---
+++ variable.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// task
+let admin;
+let name;
+
+name = "John";
+console.log(name);
+
+admin = name;
+console.log(admin);
+
+alert(admin);
+
+// task
+const EARTH = "地球";
+let currentVisiter;
+// ↓
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+// task
+const birthday = '18.04.1982'; // make uppercase?
+let age = someCode(birthday); // make uppercase? error will be occured. you should write something on someCode.
+console.log("you should use uppercase for BIRTHDAY. However should not use uppercase for age");
