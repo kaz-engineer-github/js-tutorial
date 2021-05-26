398f584682af159a5301baad9d518b6e798b7258
kaz <kazufumi2468australia@gmail.com>
2021-05-26 09:57:30 +0900

    comparison

---
+++ comparison.js
@@ -0,0 +1,17 @@
+// task
+5 > 4 // ? -> true
+"apple" > "pineapple"// ? -> false
+"2" > "12" // ? -> false
+undefined == null // ? -> true 
+undefined === null // ? -> false
+null == "\n0\n" // ? ->  false
+null === +"\n0\n" // ? ->  false
+
+// Answer
+5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
